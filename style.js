8e99be3aeddd95c6cf20b79492ddc8c9c0858364
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2017-04-15 22:33:34 -0400

    Initial commit

---
+++ style.js
@@ -0,0 +1,32 @@
+class Style {
+    constructor(style) {
+        this.position = function(_position) { return { style : "css", type : "position", position: _position } };
+        this.display = function(_display) { return { style : "css", type : "display", display : _display } };
+        this.padding = function(_padding) { return { style : "css", type : "padding", padding : _padding } };
+        this.cursor = function(_cursor) { return { style : "css", type : "cursor", cursorType : _cursor } };
+        this.border = function(_border) { return { style : "css", type : "border", border : _border } };
+        this.background = function(_background) { return { style : "css", type : "background", background : _background } };
+        this.font = function(size, family, color) { return { style : 'css', type : 'font', fontFamily : family, 'fontSize' : size, 'color' : color }};
+        this.merge = function() {
+            var keys = {};
+            for (var i=0;i<arguments.length; i++) {
+                if (arguments[i].style == "css") {
+                    if (arguments[i].type == "display") keys.display = arguments[i].display;
+                    if (arguments[i].type == "position") keys.position = arguments[i].position;
+                    if (arguments[i].type == "padding") keys.padding = arguments[i].padding + "px";
+                    if (arguments[i].type == "border") keys.border = arguments[i].border;
+                    if (arguments[i].type == "background") keys.background = arguments[i].background;
+                    if (arguments[i].type == "color") keys.color = arguments[i].color;
+                    if (arguments[i].type == "cursor") keys.cursor = arguments[i].cursorType;
+                    if (arguments[i].type == "font") {
+                        keys.color = arguments[i].color;
+                        keys.fontFamily = 'courier';//arguments[i].fontFamily;
+                        keys.fontSize = arguments[i].fontSize + 'px';
+                    }
+                }
+            }
+            return keys;
+        }
+        return this;
+    }
+}
